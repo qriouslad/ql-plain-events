c06f4bec1dcddb27023e6b9615cd405b40379cde
Bowo <wibowosulistio@gmail.com>
2019-04-01 23:06:20 +0700

    Add WP Plugin Boilerplate scaffolding and .gitignore

---
+++ includes/class-ql-plain-events-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://bowo.io
+ * @since      1.0.0
+ *
+ * @package    Ql_Plain_Events
+ * @subpackage Ql_Plain_Events/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Ql_Plain_Events
+ * @subpackage Ql_Plain_Events/includes
+ * @author     Bowo <hello@bowo.io>
+ */
+class Ql_Plain_Events_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
